b0b974ccdfb7612381388c4a7aa4ef0153abe53e
sivavignesh-960 <sp8224@srmist.edu.in>
2026-03-11 10:29:33 +0530

    Added dev

---
+++ uc2_HelloApp.java
@@ -0,0 +1,11 @@
+public class uc2_HelloApp {
+    public static void main(String[] args) {
+
+        if(args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
+    }
+}
